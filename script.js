b405aa22135628c6794bb3b7b2baccbe103fde0b
AlbertoFerreira1 <alberto.ferreira.1601@gmail.com>
2025-10-27 14:32:26 +0000

    commit final, project done

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
-<html>
-<head>
+const flipButton = document.getElementById("flipButton");
+const coin = document.getElementById("coin");
+const coinImage = document.getElementById("coinImage");
+const coinText = document.getElementById("coinText");
 
-</head>
-</html>
+function flipTheCoin() {
+    const randomNumber = Math.floor(Math.random() * 10) + 1;
+    if (randomNumber <= 5) {
+        coinImage.src ="./resources/heads.svg"
+        coinText.textContent = "Heads"
+    } else {
+        coinImage.src = "./resources/tails.svg"
+        coinText.textContent = "Tails";
+    }
+}
+
+flipButton.addEventListener('click', flipTheCoin);
+flipButton.addEventListener('click', flipTheCoin);
